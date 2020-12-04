a53bf19a40c0e18fc5e75b0d840cc35a36a4a30c
SandyMaull <sandymaulana0@gmail.com>
2020-12-04 07:18:40 +0700

    Version 0.0.3 Alpha

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -3,6 +3,6 @@
       <strong>Copyright &copy; 2020 Jango Desa - Powered By: <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
-          <b><a href="https://github.com/SandyMaull/jangodesa">Version</b> 0.0.2 Alpha</a>
+          <b><a href="https://github.com/SandyMaull/jangodesa">Version</b> 0.0.3 Alpha</a>
       </div>
   </footer>
